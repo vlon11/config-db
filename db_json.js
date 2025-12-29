85a9704256436b1d0f16dcd6c3fe477a0d2caf86
vlon11 <zjyfifa98@qq.com>
2025-12-29 20:30:18 +0800

    Add files via upload

---
+++ db_json.js
@@ -0,0 +1,16 @@
+{
+    "sites": [
+        {
+            "key": "ffzy",
+            "name": "非凡影视",
+            "api": "https://api.ffzyapi.com/api.php/provide/vod",
+            "active": true
+        },
+        {
+            "key": "dbzy",
+            "name": "豆瓣资源",
+            "api": "https://dbzy.tv/api.php/provide/vod",
+            "active": true
+        }
+    ]
+}
